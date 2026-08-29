c3111e74fe8a4faf908872a1c1a09f98073f7363
npnquang <nguyenjoe0201@gmail.com>
2026-08-29 15:04:57 +1000

    feat: register Enum type in DB

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,11 +1,24 @@
-using Gamestore.Api;
 using Gamestore.Api.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-builder.Services.AddDbContext<GameStoreDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("GameStore")));
+// this is to let the API accept and return enum values as readable string
+builder.Services.AddControllers().AddJsonOptions(options =>
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+builder.Services.AddDbContext<GameStoreDbContext>(
+    options => options
+        .UseNpgsql(
+            builder.Configuration.GetConnectionString("GameStore"),
+            npgsqlOptions =>
+            {
+                npgsqlOptions.MapEnum<UserRole>("user_role");
+                npgsqlOptions.MapEnum<Genre>("game_genre");
+            }
+        )
+        .UseSnakeCaseNamingConvention()
+);
 
 var app = builder.Build();
 
